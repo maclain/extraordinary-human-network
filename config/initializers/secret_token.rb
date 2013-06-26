7df454e14dc0b6ac8173b64ac5a0480eb5fbaf63
MacLain Christie <maclainchristie@gmail.com>
2013-06-25 23:15:22 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ExtraordinaryHumanNetwork::Application.config.secret_token = 'ff08246fd0037aa23d769847477bb829d2f59a19d5502a84f31834fd3e78c2647e9f0ffc761b4c8c350a8c9aa0176ab3699d5f36e4d36c778c1e1d21b8fbb6b8'
